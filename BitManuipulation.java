be831a9baaddfca962c020f84bfc241675165d41
Riya Raj <rrajj7674@gmail.com>
2023-09-02 11:55:11 +0530

    Add files via upload
    Day 1. Learnt about Bit Manipulation.

---
+++ BitManuipulation.java
@@ -0,0 +1,36 @@
+import java.util.*; 
+
+
+public class BitManuipulation {
+    public static void main(String[] args) {
+        // int n=5; 
+        // int pos=2; 
+        // int BitMask = 1<<pos; 
+        // int notBitMask = ~(BitMask); 
+
+        // if((BitMask & n )==0){
+        //     System.out.println("Bit was zero.");
+        // }else {
+        //     System.out.println("Bit was one.");
+        // } 
+
+        // int newNumber = BitMask | n;
+        // System.out.println(newNumber);
+
+        // int newNumber = notBitMask & n;
+        // System.out.println(newNumber); 
+
+        Scanner sc = new Scanner(System.in); 
+        int oper = sc.nextInt();
+        // oper=1 : set, oper =0 : clear
+        int n = 5; //0101 --> 0111 --> decimal ka 7
+        
+
+
+
+
+
+
+    }
+    
+}
