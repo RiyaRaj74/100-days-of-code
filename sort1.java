d1f9184593b9a585e150289cf7d7157f37d2c024
Riya Raj <rrajj7674@gmail.com>
2023-09-02 23:16:45 +0530

    DAY 1
    I learnt about Bit Manipulation today. It was fairly easy once understood well. Learnt about Get Bit, Set Bit, Clear Bit and Update Bit. 
    I also learnt Sorting. I found it interesting. Read about Bubble Sort, Selection Sort and Insertion Sort.
    I am learning a lot everyday but I need to revise more, things are slipping away too.

---
+++ sort1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class sort1 {
+    public static void main(String args[]){
+        int arr[] = {7,8,3,2,1};
+    } 
+    System.out.println(); 
+
+
+public static void main(String[] args) {
+    int arr[]  ={7,8,3,2,1};
+
+    //bubble sort ascending order
+
+    // for (int i=0; i<arr.length-1; i++) {
+    //     for(int j=0; j<arr.length-i-1; j++){
+    //         if(arr[j]> narr[j+1]){
+    //             //swap
+    //             int temp = arr[i];
+    //             arr[j]= arr[j+1];
+    //             arr[j+1]= temp; 
+
+    //         }
+    //     }
+    // }
+
+    printArray(arr);
+
+
+}
+} 
